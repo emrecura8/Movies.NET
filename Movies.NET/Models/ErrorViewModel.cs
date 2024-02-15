4fd17869fc5743676f7e116bc29812c7f7d79a57
emrecura8 <loelrinoastine@gmail.com>
2024-02-15 17:37:13 +0300

    Add project files.

---
+++ Movies.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Movies.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
